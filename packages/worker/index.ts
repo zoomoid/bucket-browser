6ed5f26c66b637ed031dd66f3961598708cbcb66
Alexander Bartolomey <github@alexanderbartolomey.de>
2024-09-25 15:12:53 +0200

    refactor: rework static handler to stream objects from bucket
    as long as hetzner cloud object storage does not support CORS directly, we proxy this
    
    Signed-off-by: Alexander Bartolomey <github@alexanderbartolomey.de>

--- packages/worker/index.ts
+++ packages/worker/index.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import { BucketIndex } from "./src/bucket-index";
 import { convertObjectListToTree } from "./src/utils";
-import cors from "cors"
-import pino from "pino"
+import cors from "cors";
+import pino from "pino";
+import { fetch } from "bun";
 
 const app = express();
 const port = 3000;
@@ -14,16 +15,16 @@ const accessKeyId = process.env.AWS_ACCESS_KEY_ID ?? "";
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY ?? "";
 
 const logger = pino({
-  level: process.env.LOG_LEVEL ?? "debug"
-})
+  level: process.env.LOG_LEVEL ?? "debug",
+});
 
 logger.debug({
   bucketName,
   bucketEndpoint: bucketEndpoint.toString(),
   bucketRegion: bucketRegion,
   accessKeyId: accessKeyId ? "***" : "",
-  secretAccessKey: secretAccessKey ? "***" : ""
-})
+  secretAccessKey: secretAccessKey ? "***" : "",
+});
 
 const lister = new BucketIndex(
   bucketName,
@@ -31,46 +32,80 @@ const lister = new BucketIndex(
   bucketRegion,
   accessKeyId,
   secretAccessKey,
-  logger,
-)
+  logger
+);
 
-lister.start()
+lister.start();
 
-app.use(cors())
+app.use(cors());
 
 app.get("/api/objects", async (req, res) => {
-  let list: Awaited<ReturnType<BucketIndex["listBucket"]>>
-  if (req.headers["cache-control"] && req.headers["cache-control"] == "no-cache") {
+  let list: Awaited<ReturnType<BucketIndex["listBucket"]>>;
+  if (
+    req.headers["cache-control"] &&
+    req.headers["cache-control"] == "no-cache"
+  ) {
     // get fresh list, otherwise serve from cache
-    list = await lister.getLatest()
+    list = await lister.getLatest();
   } else {
-    list = await lister.get()
+    list = await lister.get();
   }
 
-  const tree = convertObjectListToTree(bucketName, list)
+  const tree = convertObjectListToTree(bucketName, list);
 
-  res.setHeader("content-type", "application/json").send(tree.toJSON()).end()
+  return res
+    .setHeader("content-type", "application/json")
+    .send(tree.toJSON())
+    .end();
 });
 
-const staticRedirectPrefix = "/static/"
+const staticRedirectPrefix = "/static/";
 
-app.get("/static/:path(*)", async (req, res) => {
+app.get("/static/:path(*)", async (req: Request, res: Response) => {
   // @ts-ignore because the @types/express package does some type level stuff to extract the param, but is not aware of regex
-  const objectPath = req.params.path
+  const objectPath = req.params.path;
 
   // fully-qualified path to the bucket
-  const objectURL = new URL(bucketEndpoint)
-  objectURL.host = `${bucketName}.${bucketEndpoint.host}`
-  objectURL.pathname = objectPath
+  const objectURL = new URL(bucketEndpoint);
+  objectURL.host = `${bucketName}.${bucketEndpoint.host}`;
+  objectURL.pathname = objectPath;
 
-  logger.debug(`Redirecting request to upstream: ${objectURL.toString()}`)
+  logger.debug(`Streaming request to upstream: ${objectURL.toString()}`);
 
-  res.redirect(objectURL.toString())
-})
+  const resp = await fetch(objectURL);
+
+  if (!resp.ok) {
+    return res.send(`Failed to fetch from ${objectURL.toString()}`);
+  }
+
+  const contentType = resp.headers.get("content-type")!;
+  const contentDisposition = resp.headers.get("content-disposition")!;
+  const contentLength = resp.headers.get("content-length")!;
+
+  res
+    .setHeader("content-type", contentType)
+    .setHeader("content-disposition", contentDisposition)
+    .setHeader("content-length", contentLength);
+
+  const stream = new WritableStream({
+    write(chunk) {
+      res.write(chunk);
+    },
+    close() {
+      res.end();
+    },
+    abort(err) {
+      console.error("Streaming error:", err);
+      res.status(500).end("Internal Server Error");
+    },
+  });
+
+  await resp.body?.pipeTo(stream);
+});
 
 app.get("/healthz", (_, res) => {
-  res.send("ok")
-})
+  return res.send("ok");
+});
 
 app.listen(port, () => {
   logger.info(`Starting lister server on port ${port}`);
